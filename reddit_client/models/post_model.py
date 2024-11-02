918f13e9b3380390790d79a0a6a6e23831be4abf
jvpcms <jvpcms.engenharia@gmail.com>
2024-11-02 13:54:35 -0300

    user and post models

---
+++ reddit_client/models/post_model.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict
+
+
+class Post:
+    def __init__(
+        self,
+    ):
+        pass
+
+    @staticmethod
+    def from_dict(response: Dict[str, Any]) -> "Post":
+        return Post()
---
+++ reddit_client/models/user_model.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict
+
+
+class User:
+    def __init__(
+        self,
+    ):
+        pass
+
+    @staticmethod
+    def from_dict(response: Dict[str, Any]) -> "User":
+        return User()
