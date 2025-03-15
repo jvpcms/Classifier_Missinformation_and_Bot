88beacf8be1bca3516e421dca3aaa15e5bd01c2f
jvpcms <jvpcms.engenharia@gmail.com>
2025-03-15 16:15:59 -0300

    add web_scraping config

---
+++ web_scraping/config/env_config.py
@@ -0,0 +1,18 @@
+import os
+import dotenv
+
+
+class EnvConfig:
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def language(self) -> str:
+        env_name = "LANGUAGE"
+        dotenv.load_dotenv()
+        env_var = os.getenv(env_name)
+
+        if env_var is None:
+            raise Exception(f"Envioriment variable {env_name} not set.")
+
+        return env_var
