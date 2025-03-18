c8e51369978c9ab97bfcb239897d380a4bfc1eb5
jvpcms <jvpcms.engenharia@gmail.com>
2025-03-18 16:32:39 -0300

    update configs

--- web_scraping/config/env_config.py
+++ web_scraping/config/env_config.py
@@ -16,3 +16,14 @@ class EnvConfig:
             raise Exception(f"Envioriment variable {env_name} not set.")
 
         return env_var
+
+    @property
+    def nltk_data_path(self) -> str:
+        env_name = "NLTK_DATA_PATH"
+        dotenv.load_dotenv()
+        env_var = os.getenv(env_name)
+
+        if env_var is None:
+            raise Exception(f"Envioriment variable {env_name} not set.")
+
+        return env_var
