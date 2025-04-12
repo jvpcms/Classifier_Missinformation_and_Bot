ea169841e50269fabbb469cce1a170c14be7c691
jvpcms <jvpcms.engenharia@gmail.com>
2025-04-12 13:55:50 -0300

    new envs

--- social_media_api/config/env_config.py
+++ social_media_api/config/env_config.py
@@ -6,9 +6,7 @@ class EnvConfig:
     def __init__(self) -> None:
         pass
 
-    @property
-    def username(self) -> str:
-        env_name = "REDDIT_USERNAME"
+    def _get_env(self, env_name: str) -> str:
         dotenv.load_dotenv()
         env_var = os.getenv(env_name)
 
@@ -16,36 +14,33 @@ class EnvConfig:
             raise Exception(f"Envioriment variable {env_name} not set.")
 
         return env_var
+
+    @property
+    def username(self) -> str:
+        env_name = "REDDIT_USERNAME"
+        return self._get_env(env_name)
 
     @property
     def password(self) -> str:
         env_name = "REDDIT_PASSWORD"
-        dotenv.load_dotenv()
-        env_var = os.getenv(env_name)
-
-        if env_var is None:
-            raise Exception(f"Envioriment variable {env_name} not set.")
-
-        return env_var
+        return self._get_env(env_name)
 
     @property
     def app_id(self) -> str:
         env_name = "REDDIT_APP_ID"
-        dotenv.load_dotenv()
-        env_var = os.getenv(env_name)
-
-        if env_var is None:
-            raise Exception(f"Envioriment variable {env_name} not set.")
-
-        return env_var
+        return self._get_env(env_name)
 
     @property
     def client_secret(self) -> str:
         env_name = "REDDIT_CLIENT_SECRET"
-        dotenv.load_dotenv()
-        env_var = os.getenv(env_name)
+        return self._get_env(env_name)
 
-        if env_var is None:
-            raise Exception(f"Envioriment variable {env_name} not set.")
+    @property
+    def bluesky_username(self) -> str:
+        env_name = "BLUESKY_USERNAME"
+        return self._get_env(env_name)
 
-        return env_var
+    @property
+    def bluesky_secret(self) -> str:
+        env_name = "BLUESKY_SECRET"
+        return self._get_env(env_name)
