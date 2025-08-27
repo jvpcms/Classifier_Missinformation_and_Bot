c34f80d5ccff9cfd70e1aadbb548ae8f3b5b045f
jvpcms <jvpcms.engenharia@gmail.com>
2025-08-27 19:30:36 -0400

    add post and user to main

--- main.py
+++ main.py
@@ -13,12 +13,21 @@ def main():
         data_collector_insntance.collect_labeled_news_from_all_sources(lambda _: True)
     )
 
-    custom_logger.debug(f"#collected news: {len(collected_labaled_news)}")
-
     for news in collected_labaled_news:
-
-        custom_logger.debug(f"news: {news.title}")
         data_collector_insntance.store_labeled_news(news)
+
+        related_posts = (
+            data_collector_insntance.associate_labeled_news_with_bluesky_posts(news)
+        )
+
+        for post in related_posts:
+            user = data_collector_insntance.retrieve_associated_user_from_bluesky_post(
+                post
+            )
+            data_collector_insntance.associte_bluesky_post_with_url(post, user)
+
+            data_collector_insntance.store_bluesky_post(post)
+            data_collector_insntance.store_bluesky_user(user)
 
 
 if __name__ == "__main__":
